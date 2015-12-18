a5e6163eabb8de879be2a5ace8fc34c70e490f75
Edward Huang <ehuang@caltech.edu>
2015-12-18 16:42:18 -0600

    Fixed float rounding issues for LINCS pathways.

--- top_pathways_lincs.py
+++ top_pathways_lincs.py
@@ -18,37 +18,45 @@ def create_no_dup(lst):
     new_lst = []
     for e in lst:
         if e not in new_lst:
-            new_lst += [e]
+            new_lst.append(e)
     return new_lst
 
 if __name__ == '__main__':
     level = int(sys.argv[1])
 
-    # Translate the drugs into English.
+    # Create dictionary, keys are drug ID's, values are the English drug names.
     f = open('./data/drug_translation.txt', 'r')
     trans_dct = {}
     for line in f:
-        line = line.split()
-        trans_dct[line[1]] = line[0]
+        drug_name, drug_id = line.split()
+        trans_dct[drug_id] = drug_name
 
     print 'Extracting NCI pathways...'
     path_file = open('./data/nci_pathway.txt', 'r')
+    # List of the pathway names.
     pathnames = []
+    # nci_path_dct, keys are NCI pathway names, values are lists of genes in the
+    # corresponding pathways.
     nci_path_dct = {}
+    # A set of all of the genes over all NCI pathways.
     nci_genes = set([])
     for line in path_file:
         line = line.split('\t')
         path_name, path_gene = line[0], line[1][:-2]
+
         nci_genes.add(path_gene)
+        
         if path_name not in nci_path_dct:
             nci_path_dct[path_name] = [path_gene]
         else:
             nci_path_dct[path_name] += [path_gene]
+        
         if path_name not in pathnames:
             pathnames += [path_name]
     path_file.close()
 
-    # Extract genes from old file...
+    # Extract genes from LINCS level 3 because level 4 data is missing the gene
+    # column.
     f = open('./data/lincs_zscore.txt', 'r')
     genes = []
     for i, line in enumerate(f):
@@ -74,18 +82,19 @@ if __name__ == '__main__':
         elif genes[i-1] == '-666':
             continue
         else:
+            # Level 4 is missing the gene column.
             if level == 4:
-                gene, coeffs = genes[i-1], line[2:]
+                gene, z_scores = genes[i-1], line[2:]
             elif level == 3:
-                gene, coeffs = genes[i-1], line[3:]
-            # #NAME? is negative infinity.
-            coeffs = [inf if x == '#NAME?' else abs(float(x)) for x in coeffs]
+                gene, z_scores = genes[i-1], line[3:]
+            # #NAME? is negative infinity in the excel file.
+            z_scores = [inf if x == '#NAME?' else abs(float(x)) for x in z_scores]
             if gene not in gene_dct:
-                gene_dct[gene] = coeffs
+                gene_dct[gene] = z_scores
             else:
                 # If a gene appears twice, then we want to get the max values.
                 curr_vals = gene_dct[gene]
-                for ci, value in enumerate(coeffs):
+                for ci, value in enumerate(z_scores):
                     gene_dct[gene][ci] = max(curr_vals[ci], value)
     f.close()
     # Delete bad genes from the listself.
@@ -109,34 +118,36 @@ if __name__ == '__main__':
     temp_drug_matrix = OrderedDict({})
     for i, row in enumerate(drug_matrix):
         drug, cell_line = row[0].split('_')
-        drug, values = trans_dct[drug] + '_' + cell_line, row[1:]
-        values = map(float, values)
+        drug, z_scores = trans_dct[drug] + '_' + cell_line, row[1:]
+        z_scores = map(float, z_scores)
+
         if drug in temp_drug_matrix:
-            temp_drug_matrix[drug] += [values]
+            temp_drug_matrix[drug] += [z_scores]
         else:
-            temp_drug_matrix[drug] = [values]
+            temp_drug_matrix[drug] = [z_scores]
     drug_matrix = temp_drug_matrix
 
     for drug in drug_matrix:
-        values = drug_matrix[drug]
-        values = [np.mean(x) for x in zip(*values)]
+        z_scores = drug_matrix[drug]
+        z_scores = [np.mean(x) for x in zip(*z_scores)]
         top_gene_indices = []
-        for i, z_score in enumerate(values):
-            if z_score > Z_SCORE_MIN:
+        for i, z_score in enumerate(z_scores):
+            if z_score >= Z_SCORE_MIN:
                 top_gene_indices += [(i, z_score)]
-        # Take only the top 500 genes at most.
+        # Take only at most MAX_GENES_PER_DRUG.
         top_gene_indices = sorted(top_gene_indices, key=lambda k:k[1],
             reverse=True)[:MAX_GENES_PER_DRUG]
         top_genes = [genes[i] for i, z_score in top_gene_indices]
         drug_matrix[drug] = top_genes
 
     if level == 4:
-        outfile = open('./results/top_pathways_lincs_lvl4.txt', 'w')
+        out = open('./results/top_pathways_lincs_lvl4.txt', 'w')
     elif level == 3:
-        outfile = open('./results/top_pathways_lincs_lvl3.txt', 'w')
+        out = open('./results/top_pathways_lincs_lvl3.txt', 'w')
     # Fisher's test for every drug/cell-line and path pair.
+    total_num_genes = len(nci_genes.union(genes))
     for drug in drug_matrix:
-        fisher_dct = {}
+        fish_dct = {}
         for path_index, path in enumerate(nci_path_dct):
             path_genes = set(nci_path_dct[path])
             n = len(path_genes)
@@ -144,15 +155,15 @@ if __name__ == '__main__':
             corr_and_path = len(corr_genes.intersection(path_genes))
             corr_not_path = len(corr_genes.difference(path_genes))
             path_not_corr = len(path_genes.difference(corr_genes))
-            neither = len(nci_genes.union(genes)) - len(corr_genes.union(path_genes))
+            neither = total_num_genes - len(corr_genes.union(path_genes))
             odds_ratio, p_value = fisher_exact([[corr_and_path, corr_not_path],
                 [path_not_corr, neither]])
-            fisher_dct[(drug, path, len(corr_genes), len(path_genes), corr_and_path)] = p_value
-        sorted_fisher = sorted(fisher_dct.items(), key=operator.itemgetter(1))
+            fish_dct[(drug, path, len(corr_genes), n, corr_and_path)] = p_value
+        sorted_fisher = sorted(fish_dct.items(), key=operator.itemgetter(1))
         # Write the drug's top pathways to file.
-        for pair, score in sorted_fisher:
-            drug, path, corr, path_len, inter = pair
+        for info, score in sorted_fisher:
+            drug, path, corr_len, path_len, inter = info
             drug, cell_line = drug.split('_')
-            combo = drug + '\t' + cell_line + '\t' + path
-            outfile.write(combo + '\t' + str(score) + '\t' + str(corr) + '\t' + str(path_len) + '\t' + str(inter) + '\n')
-    outfile.close()
+            out.write('%s\t%s\t%s\t' % (drug, cell_line, path))
+            out.write('%g\t%d\t%d\t%d\n' % (score, corr_len, path_len, inter))
+    out.close()
