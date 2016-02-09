63a1fcc8da72e1e9c1b380f473eea066e1ec9909
Edward Huang <ehuang@caltech.edu>
2016-02-08 18:54:02 -0600

    Added preprocessing for L1 top pathways.

---
+++ preprocess_l1_top_pathways.py
@@ -0,0 +1,15 @@
+### Author: Edward Huang
+
+### This script does some basic preprocessing to prepare the l1 data for
+### comparison with LINCS data.
+
+if __name__ == '__main__':
+    f = open('./results/nci_pathway_ranking_name.txt', 'r')
+    out = open('./results/top_pathways_l1.txt', 'w')
+    # Write in the filler lines
+    out.write('filler\ndrug\tpath\tscore\n')
+    for line in f:
+        drug, path, score, ignore = line.strip().split('\t')
+        out.write('%s\t%s\t%s\n' % (drug.upper(), path, score))
+    out.close()
+    f.close()
